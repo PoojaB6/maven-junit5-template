9290fea9f5fe9d58082296a8deab8a8950e27f74
pooja bharti <poojabharti80@gmail.com>
2024-06-01 01:45:51 +0300

    added subtract method

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,4 +5,15 @@ public class Calculator {
         double result=firstInputValue+secondInputValue;
         return result;
     }
+    //Subtraction of two integer value
+
+    public int subtractTwoIntegerValues(int firstInputValue, int secondInputValue){
+        int result= firstInputValue-secondInputValue;
+        return result;
+    }
+    //Greater value
+    public boolean isGreater(int firstInputValue, int secondInputValue){
+        boolean result=firstInputValue > secondInputValue;
+        return result;
+    }
 }
